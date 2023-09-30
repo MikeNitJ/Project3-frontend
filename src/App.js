6597e42fd7479adec3bd9aab4492add2d8a9aa10
Renz Lacerna <renzlacerna13@gmail.com>
2023-09-30 15:22:01 -0400

    updated Search Bar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
-import axios from "axios";
 import SearchBar from "./components/SearchBar";
-import { useState } from "react";
 
 function App() {
-  const [searchResults, setSearchResults] = useState([]);
-
-  const searchSpotify = (query) => {
-    const apiUrl = `https://api.spotify.com/v1/search?q=${query}&type=track`;
-
-    axios
-      .get(apiUrl)
-      .then((response) => {
-        setSearchResults(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data from Spotify API:", error);
-      });
+  const handleSearch = async (query) => {
+    const response = await fetch(`/search?q=${query}`);
+    if (response.ok) {
+      const data = await response.json();
+      return data.results;
+    } else {
+      return [];
+    }
   };
 
   return (
     <div className="App">
       <h1> Decibel </h1>
-      <SearchBar onSearch={searchSpotify} />
-
-      <h2>Search Results:</h2>
-      <ul>
-        {searchResults.map((item, index) => (
-          <li key={index}>{item.artist}</li>
-          <li key={index}>{item.track}</li>
-        ))}
-      </ul>
+      <SearchBar onSearch={handleSearch} />
     </div>
   );
 }
