d5941ee83d240afe4857e081fc470d6b02fd731d
mikenitj <mikenitipong@hotmail.com>
2023-10-04 11:24:01 +0700

    Initialize project using Create React App

--- src/router.js
+++ src/router.js
@@ -2,25 +2,22 @@ import {
     createBrowserRouter,
     createRoutesFromElements,
     Route,
-    Redirect,
   } from "react-router-dom"
 import App from "./App"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
-// import { loginLoader, signupLoader } from "./loader"
 import Home from "./pages/Home"
-import { signInAction, signUpAction } from "./action"
 import Playlist from "./pages/Playlist"
+import PrivateRoute from './PrivateRoute';
+
 
   const router = createBrowserRouter(
     createRoutesFromElements(
     <Route path="/" element={<App />}>
         <Route path="" element={<Home/>} />
-        <Route path="/login" element={<Login/>} action={signInAction} />
-        <Route path="/signup" element={<Signup/>} action={signUpAction}/>
-        <Route path="/playlist" element={<Playlist/>}  />
-        {/* <Route path="/login" element={<Login/>} loader={loginLoader} /> */}
-        {/* <Route path="/signup" element={<Signup/>} loader={signupLoader}/> */}
+        <Route path="/login" element={<Login/>}  />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/playlist" element={<PrivateRoute />} /> 
     </Route>
     )
   )
