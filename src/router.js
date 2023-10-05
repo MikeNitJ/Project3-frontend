8b2b366b69ad19820c0ffff501775f80fe367f22
mikenitj <mikenitipong@hotmail.com>
2023-10-05 09:33:43 +0700

    Hello

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Playlist from "./pages/Playlist";
-
+console.log("HELLO");
 
 const isAuthenticated = false;
 const router = createBrowserRouter(
