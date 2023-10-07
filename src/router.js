060d07d7746117f1951ee4c8056ac979aafb610e
mikenitj <mikenitipong@hotmail.com>
2023-10-07 12:36:30 +0700

    update

--- src/router.js
+++ src/router.js
@@ -1,33 +1,4 @@
 import {
-<<<<<<< HEAD
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
-import App from "./App";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
-import Playlist from "./pages/Playlist";
-console.log("HELLO");
-
-const isAuthenticated = false;
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="" element={<Home />} />
-      <Route path="/login" element={<Login  />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route
-        path="/playlist"
-        element={isAuthenticated ? (<Playlist/>) : (<Login />)}
-      />
-    </Route>
-  )
-);
-
-export default router;
-=======
     createBrowserRouter,
     createRoutesFromElements,
     Route,
@@ -64,4 +35,4 @@ const router = createBrowserRouter(
   )
 
 export default router
->>>>>>> main
+
